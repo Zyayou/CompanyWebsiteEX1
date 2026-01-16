4b428004de42440cc384139d4b1abb46dfaa7de2
Eunkyeong Cho <74223597+Zyayou@users.noreply.github.com>
2026-01-16 23:29:59 +0900

    Delete grwind directory

--- grwind/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace grwind.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
